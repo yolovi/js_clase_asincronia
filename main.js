03a906d0ad433b467d4d6145bafff02a4e6a3101
yolovi <yolanda.lovi@outlook.es>
2025-05-12 10:33:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+function pelarPatatas() {
+    console.log("Pelar y cortar las patatas");
+}
+
+function freirPatatas() {
+    console.log(
+        "Introducimos las patatas cortadas en la sartén y que se cocinen"
+    );
+    setTimeout(() => {
+        console.log("Patatas friendose");
+    }, 10000);
+}
+
+function batirHuevos() {
+    console.log("batimos los huevos");
+}
+function escurrirPatatas() {
+    console.log("Quitamos con una espumadera las patatas de la sartén");
+}
+function mezclarIngredientes() {
+    console.log("Introducimos las patatas escurridas al bol con la cebolla y el huevo.");
+}
+function hacerUnaTortilla() {
+    pelarPatatas();
+    freirPatatas();
+    batirHuevos();
+    escurrirPatatas()
+    mezclarIngredientes();
+}
+
+//   hacerUnaTortilla()
+
+// let miPromesa = new Promise((resolve, reject) => {
+//     console.log("Te prometo que te saludaré cuando pasen 3 segundos!!");
+//     setTimeout(() => {
+//         resolve("Hola");
+//     }, 3000);
+// });
+
+// miPromesa.then((valor) => console.log(valor));
+
+// let miPromesa = new Promise((resolve, reject) => {
+//     let number = 55;
+//     console.log("te prometo que " + number + " es menor o igual que 50");
+//     if (number <= 50) {
+//         resolve(number + " es menor o igual a 50");
+//     } else {
+//         reject("mentira");
+//     }
+// });
+
+// miPromesa
+//     .then((res) => console.log(res))
+//     .catch((err) => console.error(err));
+
+
+// let friendoPatatas = new Promise((resolve) => {
+//     console.log("Aquí friendo las patatas");
+//     setTimeout(() => {
+//         resolve("Patatas freídas");
+//     }, 10000);
+// });
+
+// friendoPatatas.then((value) => {
+//     console.log(value, "Ya podemos mezclar las patatas con los huevos batidos!");
+// });
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res) => res.json()) //Es como JSON.parse() necesitamos el método json() con .then
+  .then((users) => console.log(users)) //luego decirle qué hacer con la res transformada
+  .catch((err) => console.error(err)); //o con error si es rechazada
